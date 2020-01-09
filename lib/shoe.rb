2dd489d100cb99f0bc99025002462dca13a6f9f9
Asif Kamal <asif_kamal@hotmail.com>
2020-01-09 01:17:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 class Shoe
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
